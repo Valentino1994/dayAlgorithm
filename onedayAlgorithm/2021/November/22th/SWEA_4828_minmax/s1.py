5b4353e0361b0e6bb18d67c6e766e13de6f63f7b
Geunil <valen1994@gmail.com>
2021-11-22 17:01:28 +0900

    SWEA-4828

---
+++ onedayAlgorithm/2021/November/22th/SWEA_4828_minmax/s1.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+
+for t in range(1, T+1):
+    N = int(input())
+    Array = list(map(int, input().split()))
+    # 가장 큰 수와 가장 작은 수를 구한다.
+    max_num = 0
+    min_num = 1000000
+    for i in range(N):
+        if Array[i] > max_num:
+            max_num = Array[i]
+        if Array[i] < min_num:
+            min_num = Array[i]
+    print("#{0} {1}".format(t, (max_num - min_num)))
