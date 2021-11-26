51824308e5e1413ceaa0ad6aef03631bb08fd605
Geunil <demua1994@gmail.com>
2021-11-26 18:39:45 +0900

    SWEA_4837_부분집합의합

---
+++ onedayAlgorithm/2021/November/25th/SWEA_4837_부분집합의 합/s1.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+nums = range(1, 13)
+
+def SubsetSum():
+    result = 0
+    for i in range(1 << 12):
+        total = 0
+        cnt = 0
+        for j in range(12):
+            if i & (1 << j):
+                total += nums[j]
+                cnt += 1
+        if cnt == N and total == K:
+            result += 1
+
+    return result
+
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    print("#{} {}".format(tc, SubsetSum()))
