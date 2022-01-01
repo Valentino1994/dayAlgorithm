0622ef2c13ea3d0367569cf3eb08f614f3699738
Geun Il Park <geunilpark@Geuns-MacBook-Air.local>
2022-01-01 15:37:50 +0900

    Today Algorithm

---
+++ onedayAlgorithm/2022/01_January/1st/s1.py
@@ -0,0 +1,22 @@
+def solution(N, M, K, Arr):
+
+    answer = 0
+    arr = sorted(Arr)[::-1]
+    cnt = K
+
+    while M:
+        M -= 1
+        if cnt == 0:
+            idx = 1
+            cnt = K
+        else:
+            idx = 0
+            cnt -= 1
+        answer += arr[idx]
+
+    return answer
+
+N, M, K = 5, 7, 2
+Arr = [3, 4, 3, 4, 3]
+
+print(solution(N, M, K, Arr))
