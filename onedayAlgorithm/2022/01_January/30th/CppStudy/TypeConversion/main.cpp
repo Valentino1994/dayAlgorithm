4aff2167039e6ee024edbc9adc2d746749d7b14a
Geun Il Park <demua1994@gmail.com>
2022-01-30 11:35:17 +0900

    220130 CppStudy Type

---
+++ onedayAlgorithm/2022/01_January/30th/CppStudy/TypeConversion/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double a = 2.333;
+double b = 4.324;
+string dopa = "wow wonderful";
+
+int main() {
+  int ret = (int) b / double((4 / int(a)));
+  char a = 'a';
+
+  cout << (int)a << "\n";
+  cout << ret << "\n";
+  cout << dopa.substr(0, 3) << "\n";
+  reverse(dopa.begin(), dopa.end());
+  cout << dopa << "\n";
+}
