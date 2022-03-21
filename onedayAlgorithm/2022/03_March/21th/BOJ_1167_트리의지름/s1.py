319a8419e26e3d7a25360eb3ce805f37d1ad29d5
Valentino1994 <demua1994@gmail.com>
2022-03-21 09:50:48 +0900

    BOJ 1167

---
+++ onedayAlgorithm/2022/03_March/21th/BOJ_1167_트리의지름/s1.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open("input.txt", "r")
+from itertools import combinations
+
+N = int(input())
+tree = [[[], [], []] for _ in range(N+1)]
+for _ in range(N):
+    info = list(map(int, input().split()))
+    p = info[0]
+    for i in range(1, len(info)-1, 2):
+        c = info[i]
+        d = info[i+1]
+        if p not in tree[c][0] and c not in tree[p][1]:
+            tree[p][1].append(c)
+            tree[p][2].append(d)
+            tree[c][0].append(p)
+
+leaf = []
+for i in range(len(tree)):
+    if len(tree[i][0]) == 1:
+        leaf.append(i)
+
+distance = 0
+def find_distance(root):
+
+    global distance
+
+    tmp = 0
+    q = [root]
+
+    return
+
+print(tree)
+
