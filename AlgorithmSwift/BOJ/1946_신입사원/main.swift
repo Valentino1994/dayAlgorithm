115d99a501e4651571de92cf58ec418c3103d399
Guell <demua1994@gmail.com>
2022-09-19 20:19:52 +0900

    0919

--- AlgorithmSwift/BOJ/1946_신입사원/main.swift
+++ AlgorithmSwift/BOJ/1946_신입사원/main.swift
@@ -13,16 +13,30 @@ for _ in 0..<T {
     
     let N: Int = Int(readLine()!)!
     
-    var list: [[Int]] = []
+    var list = [[Int]]()
     
     // insert는 또 되네...
+//    for _ in 0..<N {
+//        list.insert(readLine()!.split(separator: " ").map{ Int(String($0))! }, at: list.endIndex)
+//    }
+    
     for _ in 0..<N {
-        list.insert(readLine()!.split(separator: " ").map{ Int(String($0))! }, at: list.endIndex)
+        list.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
     }
+    
+    var testArray: [Int] = []
+    for i in 0..<3 {
+        testArray.append(contentsOf: (0..<3))
+    }
+    
+//    print(testArray)
     
 //    (0..<N).forEach { _ in
 //        list.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
 //    }
+    testArray.forEach { _ in
+        print("hi")
+    }
     
     let sortedList: [[Int]] = list.sorted(by: { $0[0] < $1[0] })
     
---
+++ cAlgorithm/chap01/max3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    int max;
+    printf("세 정수의 최댓값을 구합니다.\n");
+}
