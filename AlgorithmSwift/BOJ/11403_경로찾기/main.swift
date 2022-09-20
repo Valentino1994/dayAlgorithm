0256e5866c64fb5afe332eb4dd93f3743ba95086
Guell <demua1994@gmail.com>
2022-09-20 23:12:21 +0900

    0920

---
+++ AlgorithmSwift/BOJ/11403_경로찾기/main.swift
@@ -0,0 +1,49 @@
+//
+//  main.swift
+//  AlgorithmSwift
+//
+//  Created by Geunil Park on 2022/09/20.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+var graph = [[Int]]()
+
+for _ in 0..<N {
+    graph.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
+}
+
+var result = ""
+
+for i in 0..<N {
+    var visited = Array(repeating: 0, count: N)
+    
+    var stack = [Int]()
+    
+    for j in 0..<graph[i].count {
+        if graph[i][j] == 1 {
+            stack.append(j)
+        }
+    }
+    
+    while !stack.isEmpty {
+        var node = stack.removeFirst()
+        if visited[node] == 0 {
+            visited[node] = 1
+            for k in 0..<graph[node].count {
+                stack.append(k)
+            }
+        }
+    }
+    
+    result += visited.map{ String($0) }.joined(separator: " ")
+    result += "\n"
+}
+
+result.removeLast()
+
+print(N)
+print(graph)
+print(result)
