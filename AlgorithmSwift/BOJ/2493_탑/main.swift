135ac8763f2a9598c8482c198b812501c6e0eefd
Guell <demua1994@gmail.com>
2022-09-30 21:44:59 +0900

    0930

--- AlgorithmSwift/BOJ/2493_탑/main.swift
+++ AlgorithmSwift/BOJ/2493_탑/main.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+let N = Int(readLine()!)!
+let towers = readLine()!.split(separator: " ").map { Int(String($0)) }
+
+var towerIndex = N-1
+
+var answer = [Int]()
+
+while towerIndex > 0 {
+    
+    var razerIndex = towerIndex - 1
+    
+    while razerIndex > 0 {
+        if towers[razerIndex]! >= towers[towerIndex]! {
+            answer.append(razerIndex+1)
+            break
+        } else {
+            razerIndex -= 1
+        }
+    }
+    if razerIndex == 0 {
+        answer.append(0)
+    }
+    towerIndex -= 1
+}
+
+answer.append(0)
+
+answer = answer.reversed()
+
+print(answer)
