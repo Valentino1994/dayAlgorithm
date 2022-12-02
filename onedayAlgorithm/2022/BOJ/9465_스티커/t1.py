7fc916c7d9e042adf24b6f698d23c2da64bf42af
Geunil Park <77421835+Valentino1994@users.noreply.github.com>
2022-12-02 12:54:24 +0900

    1202 BOJ 9465

--- onedayAlgorithm/2022/BOJ/9465_스티커/t1.py
+++ onedayAlgorithm/2022/BOJ/9465_스티커/t1.py
@@ -1,31 +1,24 @@
 import sys
 sys.stdin = open("input.txt")
 
-def answer():
+def solution():
     N = int(input())
     board = []
     for _ in range(2):
         board.append(list(map(int, input().split(" "))))
 
+    DP = [[0] * (N+1) for _ in range(2)]
+    DP[0][1] = board[0][0]
+    DP[1][1] = board[1][0]
 
-    # 버린다는 선택지는 어떻게 하게 할 수 있을까.
-    DP_UP = [0] * N
-    DP_DOWN = [0] * N
+    for i in range(2, N+1):
+        DP[0][i] = max(DP[1][i-1], DP[1][i-2]) + board[0][i-1]
+        DP[1][i] = max(DP[0][i-1], DP[0][i-2]) + board[1][i-1]
 
-    for i in range(N):
-        if i % 2 == 0:
-            DP_UP[i] = board[0][i]
-            DP_DOWN[i] = board[1][i]
-        else:
-            DP_UP[i] = board[1][i]
-            DP_DOWN[i] = board[0][i]
-
-    print(sum(DP_UP))
-    print(sum(DP_DOWN))
-
-    return
+    return max(max(DP[0]), max(DP[1]))
 
 T = int(input())
 for _ in range(T):
-    answer()
+    answer = solution()
+    print(answer)
 
