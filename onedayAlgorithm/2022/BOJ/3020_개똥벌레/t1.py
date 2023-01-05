cfd0069b3c3bc1596e846b53d4f692dc62b3d822
Geunil Park <77421835+Valentino1994@users.noreply.github.com>
2023-01-05 21:53:04 +0900

    0105 BOJ 3020

--- onedayAlgorithm/2022/BOJ/3020_개똥벌레/t1.py
+++ onedayAlgorithm/2022/BOJ/3020_개똥벌레/t1.py
@@ -1,3 +1,22 @@
 import sys
 sys.stdin = open("input.txt")
 
+N, H = list(map(int, input().split(" ")))
+cave = []
+for _ in range(N):
+    cave.append(int(input()))
+
+heights = [0 for _ in range(H)]
+for i in range(len(cave)):
+    now_height = cave[i]
+    if i % 2 == 0:
+        for j in range(now_height):
+            heights[j] += 1
+    else:
+        for j in range(H-1, H-now_height-1, -1):
+            heights[j] += 1
+
+_min = min(heights)
+_count = heights.count(_min)
+
+print(_min, _count)
